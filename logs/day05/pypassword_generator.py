2aa6901ff452b56d1c6c0f8f57cf7cdc94d7925d
Ceci <106780203+cecirio@users.noreply.github.com>
2023-01-12 21:00:09 -0700

    Create pypassword_generator.py

---
+++ logs/day05/pypassword_generator.py
@@ -0,0 +1,77 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+password = ""
+for letter in range(1, nr_letters + 1):
+  random_letter = random.choice(letters)
+  password += random_letter
+
+for sym in range(1, nr_symbols + 1):
+  password += random.choice(symbols)
+#or
+'''
+for sym in range(1, nr_symbols +1):
+  random_symbol = random.choice(symbols)
+  password += random_symbol
+'''
+for num in range(1, nr_numbers + 1):
+  password += random.choice(numbers)
+
+#print(password)
+  
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+pass_list = list(password)
+# print(pass_list)
+random.shuffle(pass_list)
+
+# print(pass_list)
+
+strong_pass = ""
+for char in pass_list:
+  strong_pass += char
+  
+print(f"Your password is: {strong_pass}")
+
+#or
+
+'''
+password_list= []
+for letter in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for sym in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+#or
+for sym in range(1, nr_symbols +1):
+  random_symbol = random.choice(symbols)
+  password_list += random_symbol
+
+for num in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
+
+'''
+
+# if input = 4 iteration: 1, 2, 3 ONLY. Does not include last num in range.
+# To include last num in input range add 1
+
